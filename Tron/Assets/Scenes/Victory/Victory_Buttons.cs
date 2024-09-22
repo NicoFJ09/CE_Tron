dd562f8b3bead737cb86b2d3a494629d7c8a7676
NicoFJ09 <florezjnicolas@gmail.com>
2024-09-22 09:41:19 -0600

    All Items/powers working

---
+++ Tron/Assets/Scenes/Victory/Victory_Buttons.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    public int MainMenuSceneIndex = 0;
+    public int GamePlaySceneIndex = 1;
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(MainMenuSceneIndex);
+    }
+
+    public void Restart()
+    {
+        PlayerPrefs.DeleteAll();
+        SceneManager.LoadScene(GamePlaySceneIndex);
+    }
+}
